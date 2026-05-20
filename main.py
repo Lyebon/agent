da390c536075668c59cc0cd3b06635a0236314e1
Lyebon <sebas25garcia@gmail.com>
2026-05-20 17:55:28 -0300

    agent response

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("The api key is missing or not found")
+client = genai.Client(api_key=api_key)
+
+parser = argparse.ArgumentParser(description="Gemini")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+
+messages: list[types.Content] = [
+    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+]
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents=messages
+)
+
+metadata = response.usage_metadata
+if metadata is None:
+    raise RuntimeError("The API request fail to fetch the response")
+print(
+    f"Prompt tokens: {metadata.prompt_token_count}\nResponse tokens: {metadata.candidates_token_count}"
+)
+
+print(response.text)
+
+
+
+
+def main():
+    print("Hello from agent!")
+
+
+if __name__ == "__main__":
+    main()
