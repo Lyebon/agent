588566a088b18838220e6287bc26ca28b3ace91c
Lyebon <sebas25garcia@gmail.com>
2026-05-23 14:43:53 -0300

    Test for get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -9,6 +9,14 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if os.path.isdir(target_dir):
             return f'Error: "{directory}" is not a directory'
-        return f'Success: "{directory}" is within the working directory'
+        dir_content = os.listdir(working_dir_abs)
+        result = ""
+        for dir in dir_content:
+            if result == "":
+                result += f"- {dir}: file_size={os.path.getsize(dir)} bytes, is_dir={os.path.isdir(dir)}"
+            else:
+                result += f"\n- {dir}: file_size={os.path.getsize(dir)} bytes, is_dir={os.path.isdir(dir)}"
+        return result
+        
     except Exception as e:
         print(f"Error found: {e}")
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -8,6 +8,13 @@ class TestGet_files(unittest.TestCase):
         print(get_files_info("calculator", "../"))
         print(get_files_info("calculator", "main.py"))
 
+    def test_get_files_path(self):
+        problem = get_files_info("calculator", ".")
+        result = '''- main.py: file_size=719 bytes, is_dir=False
+- tests.py: file_size=1331 bytes, is_dir=False
+- pkg: file_size=44 bytes, is_dir=True
+'''
+        self.assertEqual(problem, result)
 
 if __name__ == "__main__":
     unittest.main()
