0665f228516286b257f86f7e0fe0fb7ac5293ab7
Lyebon <sebas25garcia@gmail.com>
2026-05-28 15:19:43 -0300

    call_functions updated

--- functions/call_functions.py
+++ functions/call_functions.py
@@ -4,5 +4,8 @@ from google.genai import types
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_write_files,
+        schema_get_files_content,
+        schema_run_python_files
         ],
 )
